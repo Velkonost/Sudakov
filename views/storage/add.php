8e5a30b262805f0059d111b50902e7874e2ed39c
fastvepik <andrey.yurin55@gmail.com>
2017-07-18 19:43:18 +0300

    fixed 3d edit

--- views/storage/add.php
+++ views/storage/add.php
@@ -268,8 +268,14 @@ $user = Yii::$app->user->identity;
         margin: 0 !important;
         height: 0 !important;
     }
-
-</style>
+	input::-webkit-outer-spin-button,
+	input::-webkit-outer-spin-button,
+	input::-webkit-inner-spin-button {
+/* display: none; <- Crashes Chrome on hover */
+	-webkit-appearance: none;
+	margin: 0; /* <— Apparently some margin are still there even though it's hidden */
+	}
+	</style>
 <section>
 <table class="inputTable" >
     <tbody  style="min-width: 1170px; width: 1170px; max-width: 1170px">
@@ -1597,7 +1603,9 @@ if(pieces[pieces.length-1] == 'true') {
 // $("#value").prop('disabled', 'disabled');
 
 document.getElementById('selectStatus').style.color = "#CCCCCC";
+document.getElementById('selectStatus_send').value = "null";
 $("#selectStatus").prop('disabled', 'disabled');
+
 
 document.getElementById('type_img_name').value = "empty.jpg";
 document.getElementById('name_img_name').value = "empty.jpg";
@@ -1625,6 +1633,8 @@ var showGreyNamesAllowed = true;
 
 var type_selected = false;
 var name_selected = false;
+
+var testStatus = false;
 
 var isSelectedName2;
 
@@ -1868,12 +1878,14 @@ function selectType(type, name, desc, src) {
         document.getElementById('value').setAttribute('onfocus', 'this.blur()');
         document.getElementById('value').style.color = "#e2e2e2";
 
-        document.getElementById('selectStatus_send').value='null';
+        
         //document.getElementById('selectStatus').value = 'null';
-        document.getElementById("name_title_send").value = "null"; 
+        document.getElementById("name_title_send").value = "null";
+		
         document.getElementById('selectStatus').style.color = "#CCCCCC";
         $("#selectStatus").prop('disabled', 'disabled');
-        
+        document.getElementById('selectStatus_send').value='null';
+		
         document.getElementById('nonselected_name').setAttribute('class', '');
         document.getElementById('selected_name').setAttribute('class', 'hidden');
 
@@ -1891,13 +1903,17 @@ function selectType(type, name, desc, src) {
 
         $("#value").removeAttr("onfocus");
         document.getElementById('value').style.color = "";
-       document.getElementById('selectStatus_send').value=document.getElementById('selectStatus').value;
+		document.getElementById('selectStatus_send').value=document.getElementById('selectStatus').value;
         document.getElementById("selectStatus").style.border = "0px solid #fff8ca";
         document.getElementById("selectStatus").style.borderRadius = "0px";
         
         document.getElementById("name_title_send").value = ""; 
-        $("#selectStatus").removeAttr("disabled");
-        document.getElementById('selectStatus').style.color = "#3d3d3d";
+		if(testStatus == true){
+		//	$("#selectStatus option[value='']").css("display","block");
+		//	$("#selectStatus").removeAttr("disabled");
+		//	document.getElementById('selectStatus').style.color = "#3d3d3d";
+        //document.getElementById('selectStatus_send').value=document.getElementById('selectStatus').value;
+		}
         
         usingName = true;
         
@@ -2173,12 +2189,27 @@ function checkFrom(id1, id2){
     }
 
     if(document.getElementById("selectFrom").value == "") { 
+	
+		
+		$("#selectStatus").removeAttr("disabled");
+		document.getElementById('selectStatus').style.color = "#3d3d3d";
+        document.getElementById('selectStatus_send').value=document.getElementById('selectStatus').value;
+		
+		
         document.getElementById("selectTo").value = "";
         // $('#selectTo').removeAttr('disabled');
         $("#selectTo option").css("display","block");
         document.getElementById('selectTo').style.color = "";
+		
+		testStatus = false;
     } else if(document.getElementById("selectFrom").value == "Поставщик") {
 
+		
+		$("#selectStatus").removeAttr("disabled");
+		document.getElementById('selectStatus').style.color = "#3d3d3d";
+        document.getElementById('selectStatus_send').value=document.getElementById('selectStatus').value;
+		
+		
         $("#selectTo option[value='Склад']").css("display","block");
         document.getElementById("selectOperation").value = "Приход";
         // document.getElementById("selectOperation").setAttribute('disabled', 'disabled');
@@ -2195,11 +2226,19 @@ function checkFrom(id1, id2){
         // document.getElementById("selectTo").setAttribute('disabled', 'disabled');
         $("#selectTo option").css("display","none");
         $("#selectTo option[value='Склад']").css("display","block");
+		
+		
         document.getElementById('selectTo').style.color = "#CCCCCC";
 
+		testStatus = true;
     } else if(document.getElementById("selectFrom").value == "Склад") {
         $("#selectTo option[value='Склад']").css("display","none");
-
+		document.getElementById('selectStatus').style.color = "#CCCCCC";
+        $("#selectStatus").prop('disabled', 'disabled');
+        document.getElementById('selectStatus_send').value='null';
+		
+		testStatus = false;
+		
         document.getElementById("selectOperation").value = "Расход";
         // document.getElementById("selectOperation").setAttribute('disabled', 'disabled');
         $("#selectOperation option").css("display","none");
@@ -2212,7 +2251,12 @@ function checkFrom(id1, id2){
         document.getElementById('selectTo').style.color = "";
 
     } else {
-
+		
+		
+		$("#selectStatus").removeAttr("disabled");
+		document.getElementById('selectStatus').style.color = "#3d3d3d";
+        document.getElementById('selectStatus_send').value=document.getElementById('selectStatus').value;
+		
         document.getElementById("selectTo").value = "Склад";
         // document.getElementById("selectTo").setAttribute('disabled', 'disabled');
         $("#selectTo option").css("display","none");
@@ -2222,6 +2266,7 @@ function checkFrom(id1, id2){
         // $('#selectOperation').removeAttr('disabled');
         $("#selectOperation option").css("display","block");
         document.getElementById('selectOperation').style.color = "";
+		//testStatus = true;
     }
 
 }
